b6f5f2e29882e7a7d45bb43f875789b1c9f6f025
Giorgio Piazza <gio.piazza97@gmail.com>
2021-11-26 16:06:17 +0100

    Add supervisor

--- src/main/java/com/middleware/project3/Actor.java
+++ src/main/java/com/middleware/project3/Actor.java
@@ -1,5 +1,7 @@
 package com.middleware.project3;
 
+import java.util.Optional;
+
 import akka.actor.AbstractActor;
 
 public class Actor extends AbstractActor {
@@ -8,15 +10,37 @@ public class Actor extends AbstractActor {
 		return receiveBuilder().match(Message.class, this::onMessage).build();
 	}
 	
-	private void onMessage(Message message) {
-		System.out.println("Received " + message.getId() + ", sleeping...");
+	private void onMessage(Message message) throws Exception {
+		System.out.println("Received " + message.getId());
+		
+		int randInt = (int) (Math.random() * 4 + 1);
+		
+		if (randInt > 3) {
+			System.out.println("Exception " + message.getId());
+			//throw new Exception();
+		}
 		
 		try {
-			Thread.sleep(((int) (Math.random() * 4 + 1)) * 1000);
+			Thread.sleep(randInt * 1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
 		System.out.println("Finished " + message.getId());
+	}
+	
+	@Override
+	public void preRestart(Throwable reason, Optional<Object> message) {
+		System.out.println("Restarting...");
+		
+		// Pay attention to infinite loops!
+		// https://stackoverflow.com/questions/13542921/akka-resending-the-breaking-message
+		// FIXME Patch orElse == null
+		getContext().getSelf().tell(message.orElse(null), getContext().getSender());
+	}
+	
+	@Override
+	public void postRestart(Throwable reason) {
+		System.out.println("...now restarted!");	
 	}
 }
--- src/main/java/com/middleware/project3/MainClass.java
+++ src/main/java/com/middleware/project3/MainClass.java
@@ -3,13 +3,27 @@ package com.middleware.project3;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.actor.SupervisorStrategy;
 import akka.routing.BalancingPool;
+import akka.actor.OneForOneStrategy;
+import akka.japi.pf.DeciderBuilder;
+import java.time.Duration;
 
 public class MainClass {
-
+	
+	public static int POOL_ACTORS = 2;
+	
+	public static int SUPERVISOR_RETRIES = 10;
+	public static int SUPERVISOR_PERIOD = 1;
+	
 	public static void main(String[] args) {
 		ActorSystem sys = ActorSystem.create("System");
-		ActorRef router = sys.actorOf(new BalancingPool(5).props(Props.create(Actor.class)), "Router");
+
+		SupervisorStrategy strategy = new OneForOneStrategy(SUPERVISOR_RETRIES,
+				Duration.ofMinutes(SUPERVISOR_PERIOD),
+				DeciderBuilder.match(Exception.class, e -> SupervisorStrategy.restart()).build());
+		
+		ActorRef router = sys.actorOf(new BalancingPool(POOL_ACTORS, strategy, "pool-dispatcher").props(Props.create(Actor.class)), "Router");
 
 		for (int i = 0; i < 12; i++) {
 			router.tell(new Message(i), ActorRef.noSender());
