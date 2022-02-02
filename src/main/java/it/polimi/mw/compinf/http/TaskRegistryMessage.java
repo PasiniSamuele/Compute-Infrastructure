ba33416f7926179769be2e1001ca24261c8cb1e9
Samuele Negrini <samuele.negrini@mail.polimi.it>
2022-02-02 17:45:01 +0100

    Fix cbor serialization

--- src/main/java/it/polimi/mw/compinf/http/TaskRegistryMessage.java
+++ src/main/java/it/polimi/mw/compinf/http/TaskRegistryMessage.java
@@ -3,6 +3,7 @@ package it.polimi.mw.compinf.http;
 import akka.NotUsed;
 import akka.http.javadsl.model.sse.ServerSentEvent;
 import akka.stream.javadsl.Source;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import it.polimi.mw.compinf.tasks.CborSerializable;
 import it.polimi.mw.compinf.tasks.CompressionTask;
 
@@ -10,7 +11,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 public interface TaskRegistryMessage {
-    class GenericMessage implements Serializable {
+    class GenericMessage implements CborSerializable {
         private final String message;
 
         public GenericMessage(String message) {
@@ -22,7 +23,7 @@ public interface TaskRegistryMessage {
         }
     }
 
-    class CreateCompressionMessage implements Serializable {
+    class CreateCompressionMessage implements CborSerializable {
         private final CompressionTask compressionTask;
 
         public CreateCompressionMessage(CompressionTask compressionTask) {
@@ -34,7 +35,7 @@ public interface TaskRegistryMessage {
         }
     }
 
-    class CreateSSEMessage implements Serializable {
+    class CreateSSEMessage implements CborSerializable {
         private final UUID uuid;
 
         public CreateSSEMessage(UUID uuid) {
@@ -46,7 +47,7 @@ public interface TaskRegistryMessage {
         }
     }
 
-    class GetSSEMessage implements Serializable {
+    class GetSSEMessage implements CborSerializable {
         private final Source<ServerSentEvent, NotUsed> source;
 
         public GetSSEMessage(Source<ServerSentEvent, NotUsed> source) {
@@ -58,9 +59,13 @@ public interface TaskRegistryMessage {
         }
     }
 
-    class TaskExecutedMessage implements CborSerializable, Serializable {
+    class TaskExecutedMessage implements CborSerializable {
         private final UUID uuid;
 
+        // Note: annotation is needed since the constructor has a single parameter.
+        // For more info, check here:
+        // https://doc.akka.io/docs/akka/current/serialization-jackson.html#constructor-with-single-parameter
+        @JsonCreator
         public TaskExecutedMessage(UUID uuid) {
             this.uuid = uuid;
         }
